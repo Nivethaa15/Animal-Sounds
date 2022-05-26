8b186a4a7c43b856b63770be694f8898bec6ea85
Nivethaa15 <91970120+Nivethaa15@users.noreply.github.com>
2022-05-26 23:00:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
  <!DOCTYPE html>
 <html>
     <head>
-    <title>Animal Sound Identification</title>
+    <title>Guess The Animal</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -20,8 +20,8 @@
                 </div>
                 <div class="col-xs-3">
                     <div class="gameTitle">
-                        <div>Animal</div>
-                        <div>Sounds</div>
+                        <div>Guess</div>
+                        <div>The Animal</div>
                     </div>
                     <img  class="hexagonBlue2" src="image/svg/hexagonBlue2.png">
                 </div>
@@ -45,7 +45,7 @@
                 </div>
                 <div class="col-xs-3" id="help">
 					<div class="helpTitle">
-                        <div>POMOĆ!</div>
+                        <div>Help you</div>
                     </div>
 					<img  class="hexagonBlue2" src="image/svg/hexagonBlue2.png">
                 </div>
@@ -63,16 +63,16 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" id="myModalLabel">Igra je završena!</h4>
+                    <h4 class="modal-title" id="myModalLabel">Game Ends!</h4>
                 </div>
                 <div class="modal-body">
                     <form method="post" action="php/highscore.php" id="highForm">
                         <div class="form-group">
-                            <input type="text" class="form-control" id="playerName" name="name" placeholder="Upiši ime">
+                            <input type="text" class="form-control" id="playerName" name="name" placeholder="Number of Points">
                             <input type="text" class="form-control" id="playerPoints" name="points">
                         </div>
                         <div class="form-group">
-							<button type="submit" class="btn btn-default" id="saveHigh">Spremi</button>
+							<button type="submit" class="btn btn-default" id="saveHigh">Save</button>
 						 </div>
                     </form>
                     <span id="phpAppend"></span>
@@ -80,7 +80,7 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button id="modalButton" type="button" class="btn btn-primary">Ponovno pokreni igru</button>
+                    <button id="modalButton" type="button" class="btn btn-primary">Submit</button>
                 </div>
                 </div>
             </div>
